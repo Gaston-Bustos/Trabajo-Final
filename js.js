717c07a6071cfc72b409896d9ba3ff95043cd7c4
Gastón B <108241763+Gaston-Bustos@users.noreply.github.com>
2022-08-02 22:49:09 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,56 @@
+
+let genero;
+let nombre;
+genero= window.prompt ("De acuerdo a la identidad de genero ¿Se considera mujer/varon/no binario? (escribir en minuscula)")
+nombre= window.prompt ("¿Como te llamas?")
+
+if (genero== "varon") {
+  document.querySelector("#saludo").innerHTML =( "Bienvenido " + nombre); 
+} else if (genero== "mujer") {
+  document.querySelector("#saludo").innerHTML =( "Bienvenida " + nombre);
+} else {
+  document.querySelector("#saludo").innerHTML =( "Bienvenide " + nombre);
+}
+
+//carrusel
+const slides = document.querySelectorAll(".imagen");
+slides.forEach((imagen, indx) => {
+    imagen.style.transform = `translateX(${indx * 100}%)`;
+  });
+
+const nextSlide = document.querySelector(".btn-next");
+
+let curSlide = 0;
+let maxSlide = slides.length - 1;
+
+nextSlide.addEventListener("click", function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+const prevSlide = document.querySelector (".btn-prev");
+
+prevSlide.addEventListener ("click", function(){
+    if (curSlide== 0) {
+        curSlide= maxSlide;
+    } else {
+        curSlide--;
+    }
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+      });
+    }); 
+
+//https://morioh.com/p/5429543c4a07
+   
+
+/* codigo */
+
+
